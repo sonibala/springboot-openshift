c9746cb765cd80f60b735d42723080c4575c45b4
Sonia Balamurugan <sonia.balamurugan@perficient.com>
2020-06-29 18:41:32 +0530

    openshift initial commit

---
+++ src/test/java/com/example/springbootopenshiftdemo/SpringbootOpenshiftdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootopenshiftdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOpenshiftdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
